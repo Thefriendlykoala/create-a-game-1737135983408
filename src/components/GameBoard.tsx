a1700a00a2062765eeafb358eed0f8fcb61d5c98
Thefriendlykoala <91312002+Thefriendlykoala@users.noreply.github.com>
2025-01-18 04:46:26 +1100

    Add src/components/GameBoard.tsx

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameBoard = () => {
+  return (
+    <div className="game-board">
+      {/* Game board logic and rendering */}
+    </div>
+  );
+};
+
+export default GameBoard;
